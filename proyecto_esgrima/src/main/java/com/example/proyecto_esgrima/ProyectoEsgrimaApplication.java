3e2753bf0ce537bfabb3ed1793092ae9f3f094eb
samuel18c <adam24scarrasco@inslaferreria.cat>
2026-02-17 19:03:07 +0100

    prueba

---
+++ proyecto_esgrima/src/main/java/com/example/proyecto_esgrima/ProyectoEsgrimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_esgrima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEsgrimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEsgrimaApplication.class, args);
+	}
+
+}
---
+++ proyecto_esgrima/src/test/java/com/example/proyecto_esgrima/ProyectoEsgrimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_esgrima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEsgrimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
