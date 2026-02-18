0014f4a488d5c50718b82829c3a7e8e25a0d16af
samuel18c <adam24scarrasco@inslaferreria.cat>
2026-02-18 18:58:03 +0100

    entity userLogin creada

---
+++ proyecto_esgrima/src/main/java/com/example/proyecte_esgrima/model/Role.java
@@ -0,0 +1,8 @@
+package com.example.proyecte_esgrima.model;
+
+public enum Role {
+
+	ROLE_USER,
+    ROLE_ADMIN
+
+}
---
+++ proyecto_esgrima/src/main/java/com/example/proyecte_esgrima/model/UserLogin.java
@@ -0,0 +1,68 @@
+package com.example.proyecte_esgrima.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+
+@Document
+public class UserLogin {
+	
+	@Id
+	private String id;
+	
+	@Indexed(unique = true)
+	private String username;
+	
+	private String password;
+	
+	private Role role;
+
+	public UserLogin(String id, String username, String password, Role role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	public UserLogin() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	
+
+}
