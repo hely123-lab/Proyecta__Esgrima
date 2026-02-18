b3ce0ef883537c9d44ae49572cbff16e5cd995aa
samuel18c <adam24scarrasco@inslaferreria.cat>
2026-02-18 19:04:40 +0100

    Repository de UserLogin

--- proyecto_esgrima/src/main/java/com/example/proyecto_esgrima/repository/UserLoginRepository.java
+++ proyecto_esgrima/src/main/java/com/example/proyecto_esgrima/repository/UserLoginRepository.java
@@ -1,5 +1,11 @@
 package com.example.proyecto_esgrima.repository;
 
-public interface UserLoginRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.proyecte_esgrima.model.UserLogin;
+
+@Repository
+public interface UserLoginRepository extends MongoRepository<UserLogin, String>{
 
 }
