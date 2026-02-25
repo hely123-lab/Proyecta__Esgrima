448f2296147745715e3faf1bb09560b97d9c8280
samuel18c <adam24scarrasco@inslaferreria.cat>
2026-02-25 17:37:41 +0100

    interficie reservaService

---
+++ proyecto_esgrima/src/main/java/com/example/proyecte_esgrima/services/ReservaService.java
@@ -0,0 +1,15 @@
+package com.example.proyecte_esgrima.services;
+
+import com.example.proyecte_esgrima.model.Reserva;
+import com.example.proyecte_esgrima.model.dto.ReservaRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ReservaService {
+    Reserva crearReserva(String esgrimista1Id, ReservaRequest request);
+    Reserva getById(String id);
+    List<Reserva> getHistorialByUsuari(String usuariId);
+    List<Reserva> getAll();
+    List<Reserva> getDisponibilitatPista(String pistaId, LocalDateTime inici, LocalDateTime fi);
+    Reserva cancellarReserva(String id, String usuariId);
+}
